e10f045382ba4dbfe37d474bf672be9a7a5e66e0
kailafitz <mikhailafitzpatrick@yahoo.com>
2025-08-27 19:49:45 +1000

    Icon added to Our Story heading

--- app/our-story/page.tsx
+++ app/our-story/page.tsx
@@ -11,7 +11,24 @@ const AboutUs: React.FC = () => {
                 className="grid grid-cols-1 md:grid-cols-2 text-foreground font-body gap-y-10 md:gap-y-0 md:gap-x-20"
             >
                 <div>
-                    <h1 className="text-large uppercase font-medium mb-4 tracking-wide">
+                    <h1 className="text-large uppercase font-medium mb-4 tracking-wide flex items-center">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="lucide lucide-sparkles-icon lucide-sparkles inline w-5 mr-1.5"
+                        >
+                            <path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z" />
+                            <path d="M20 2v4" />
+                            <path d="M22 4h-4" />
+                            <circle cx="4" cy="20" r="2" />
+                        </svg>
                         Our Story
                     </h1>
                     <h2 className="text-5xl font-medium">
